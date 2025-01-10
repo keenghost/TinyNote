a3e7243d3df01a274e845542d2da3daa44945f7b
keenghost <keenghost@163.com>
2025-01-10 21:00:48 +0800

    chore: pkg-web polish path alias

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@src': fileURLToPath(new URL('./src', import.meta.url)),
       '@assets': fileURLToPath(new URL('./assets', import.meta.url)),
     },
   },

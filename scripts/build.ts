741b7fcca5cc63badc8a95c32bdb725a74e0a15e
keenghost <keenghost@163.com>
2025-01-14 18:56:52 +0800

    chore: remove useless build scripts

--- scripts/build.ts
+++ scripts/build.ts
@@ -24,25 +24,5 @@ for (const proj of projs) {
 
 for (const proj of projs) {
   child_process.execSync('pnpm build', { cwd: `packages/${proj}`, stdio: 'inherit' })
-
-  if (fs.existsSync(`packages/${proj}/dist/package.json`)) {
-    const json = fs.readFileSync(`packages/${proj}/dist/package.json`, 'utf8')
-    const pkg = JSON.parse(json)
-    const deps = pkg.dependencies || []
-
-    for (const dep in deps) {
-      if (deps[dep].startsWith('workspace:')) {
-        const pkgname = dep.replace('baidu-netdisk-', '')
-        const pkgpath = `packages/${pkgname}/package.json`
-        const pkgjson = fs.readFileSync(pkgpath, 'utf8')
-        const pkgpkg = JSON.parse(pkgjson)
-        const pkgversion = pkgpkg.version
-        deps[dep] = deps[dep].replace('workspace:', '').replace('*', pkgversion)
-      }
-    }
-
-    fs.writeFileSync(`packages/${proj}/dist/package.json`, JSON.stringify(pkg, null, 2), 'utf8')
-  }
-
   console.log(pico.white(`\n${pico.bgGreen(' OK ')} Project ${pico.blue(proj)} built.\n\n`))
 }
